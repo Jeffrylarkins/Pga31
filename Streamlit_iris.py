789809e3cccea130181ad694cfe6ce4cdeae2787
Jeffrylarkins <84015795+Jeffrylarkins@users.noreply.github.com>
2024-03-01 11:28:10 +0530

    Add files via upload

---
+++ Streamlit_iris.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pickle
+
+st.title('Iris Prediction')
+
+a = st.number_input('Enter Sepal Length ')
+b = st.number_input('Enter Sepal Width ')
+c = st.number_input('Enter Petal Length ')
+d = st.number_input('Enter Petal Width ')
+
+process = st.selectbox('Choose the operations : ', ('Addition', 'Subtraction', 'Multiplication', 'Division'))
+
+if st.button('Good to execute??'):
+    if process=='Addition':
+        st.write(a+b+c+d)
+    elif process=='Multiplication':
+        st.write(a*b*c*d)
+    else:
+        st.write('Development in process')
